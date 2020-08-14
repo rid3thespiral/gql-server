ce5ae5411b6ca71bc267e3c4bb0e5d86da79715d
rid3thespiral <davidebattaglia1993@gmail.com>
2020-08-14 12:47:00 +0200

    server up to running

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var express_gql = require('express-graphql');
+const {graphqlHTTP} = require('express-graphql');
 var {buildSchema} = require('graphql');
 
 // GraphQL Schema
@@ -11,10 +11,16 @@ var schema = buildSchema(`
 
 // Root resolver
 var root = {
-    message: () => 'Hello World!';
+    message: () => 'Hello World!'
 };
 
 // Create an express server and a GQL endpoint
 var app = express();
-app.use('/graphql')
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true //for the UI
+}));
+
+app.listen(4000, () => console.log('Express GQL Server now Running on localhost:4000/graphql!'));
 
